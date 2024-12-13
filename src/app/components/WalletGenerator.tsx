49a75247488d2e122a147ea60da6e1ebd73c0d1b
Shashwat Singh <shashwat123student@gmail.com>
2024-12-14 01:08:02 +0530

    cleanup

--- src/app/components/WalletGenerator.tsx
+++ src/app/components/WalletGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { generateWallet, EthereumKeyPair, SolanaKeyPair } from '../utils/walletUtils';
 
 interface WalletGeneratorProps {
